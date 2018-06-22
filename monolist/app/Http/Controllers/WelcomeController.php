8f1f0a5de0237fd849e2caf55589c175c3ecd9f0
yui.honda <yui.a.honda@rakuten.com>
2018-06-22 01:52:43 +0000

    user registration

--- monolist/app/Http/Controllers/WelcomeController.php
+++ monolist/app/Http/Controllers/WelcomeController.php
@@ -20,3 +20,4 @@ class WelcomeController extends Controller
         return view('welcome');
     }
 }
+
--- monolist/resources/views/commons/error_messages.blade.php
+++ monolist/resources/views/commons/error_messages.blade.php
@@ -2,4 +2,5 @@
     @foreach ($errors->all() as $error)
         <div class="alert alert-warning">{{ $error }}</div>
     @endforeach
+    <li><a href="{{ route('signup.get') }}">新規登録</a></li>
 @endif
